f4e08b7205323a72799673ecab63668b24b5c55c
Filipe Lukebana <filipelukebana@gmail.com>
2022-10-12 11:28:59 -0300

    🪲 Bug Fixe: Open unsupported url scheme to external apps. 🔥

--- lib/webpage.dart
+++ lib/webpage.dart
@@ -19,6 +19,7 @@ class WebpageState extends State<Webpage> {
 
   @override
   Widget build(BuildContext context) {
+    //Variable to store the webviewController
     WebViewController? controller;
     return WillPopScope(
         child: Scaffold(
@@ -30,11 +31,14 @@ class WebpageState extends State<Webpage> {
                     initialUrl: 'https://yodadex.finance/',
                     javascriptMode: JavascriptMode.unrestricted,
                     onWebViewCreated: (webviewController) {
+                      //Here we capture the webcontroller and store it on our own variable to use it later.
                       controller = webviewController;
                     },
                     navigationDelegate: (NavigationRequest request) async {
                       //Validating the url and if is an app link, launch an external application
-                      if (request.url.contains(".app.link")) {
+                      if (request.url.contains(".app.link") ||
+                          request.url.contains("walletconnect") ||
+                          request.url.contains("trustwallet")) {
                         await launchUrlString(
                           request.url,
                           mode: LaunchMode.externalApplication,
@@ -52,10 +56,13 @@ class WebpageState extends State<Webpage> {
           ),
         ),
         onWillPop: () async {
+          //Check if we have the webviewController
           if (controller != null) {
+            //Check if there is a anivagation history from where we can go back
             if (await controller!.canGoBack()) {
               controller!.goBack();
             } else {
+              //If there is no navigation history, it means that the user wants to leave the app
               final shouldPop = await showDialog<bool>(
                 context: context,
                 builder: (context) {
