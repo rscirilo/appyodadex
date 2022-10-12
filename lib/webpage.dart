318cd3a0693676b95bb8806d774cb23d34b57eca
Filipe Lukebana <filipelukebana@gmail.com>
2022-10-12 11:20:11 -0300

    �🚀 Feature: Send back button events to the webview and Ask user before quiting the App

--- lib/webpage.dart
+++ lib/webpage.dart
@@ -19,37 +19,81 @@ class WebpageState extends State<Webpage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: WebView(
-                initialUrl: 'https://yodadex.finance/',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (controller) {
-                  controller.runJavascript("""
-
-
-                """);
-                },
-                navigationDelegate: (NavigationRequest request) async {
-                  //Validating the url and if is an app link, launch an external application
-                  if (request.url.contains(".app.link")) {
-                    await launchUrlString(
-                      request.url,
-                      mode: LaunchMode.externalApplication,
-                    );
-                    return NavigationDecision.prevent;
-                  } else {
-                    return NavigationDecision.navigate;
-                  }
-                },
-              ),
+    WebViewController? controller;
+    return WillPopScope(
+        child: Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  child: WebView(
+                    initialUrl: 'https://yodadex.finance/',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (webviewController) {
+                      controller = webviewController;
+                    },
+                    navigationDelegate: (NavigationRequest request) async {
+                      //Validating the url and if is an app link, launch an external application
+                      if (request.url.contains(".app.link")) {
+                        await launchUrlString(
+                          request.url,
+                          mode: LaunchMode.externalApplication,
+                        );
+                        return NavigationDecision.prevent;
+                      } else {
+                        // print("Link Aqui ${request.url}");
+                        return NavigationDecision.navigate;
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-    );
+        onWillPop: () async {
+          if (controller != null) {
+            if (await controller!.canGoBack()) {
+              controller!.goBack();
+            } else {
+              final shouldPop = await showDialog<bool>(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text(
+                      'Pretende sair da aplicação?',
+                      style: TextStyle(color: Color.fromARGB(255, 57, 22, 63)),
+                    ),
+                    actionsAlignment: MainAxisAlignment.spaceBetween,
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context, true);
+                        },
+                        child: const Text(
+                          'Sim',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 170, 57, 190)),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context, false);
+                        },
+                        child: const Text(
+                          'Não',
+                          style:
+                              TextStyle(color: Color.fromARGB(255, 57, 22, 63)),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+              return shouldPop!;
+            }
+          } else {}
+          return false;
+        });
   }
 }
